4859d0b07460e7d48dd4554b7fd6f7e0184d65e6
SuperGluedGIT <sparkn@mac.com>
2024-07-29 20:27:39 -0400

    Connected to GitHub

--- gui.py
+++ gui.py
@@ -5,8 +5,8 @@ label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip="enter todo")
 add_button = sg.Button("Add")
 
-
-window = sg.Window('My To-Do App', layout=[[label], [input_box, add_button]])
+window = sg.Window('My To-Do App', layout=[[label], [input_box, add_button]]
+                   )
 window.read()
 print("Hello")
 window.close()
