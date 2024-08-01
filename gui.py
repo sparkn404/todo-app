5c3161670ab374a1c9fc6c79f05e013ed8f787ae
SuperGluedGIT <sparkn@mac.com>
2024-08-01 13:11:49 -0400

    Implement Edit button and refreshes.

--- gui.py
+++ gui.py
@@ -4,9 +4,13 @@ import FreeSimpleGUI as sg
 label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip="enter todo", key="todo")
 add_button = sg.Button("Add")
+list_box = sg.Listbox(values=functions.get_todos(), key="todos", enable_events=True, size=[44,10])
+edit_button = sg.Button("Edit")
 
 window = sg.Window('My To-Do App',
-                   layout=[[label], [input_box, add_button]],
+                   layout=[[label],
+                           [input_box, add_button],
+                           [list_box, edit_button]],
                    font=('Helvetica', 20))
 
 while True:
@@ -14,11 +18,24 @@ while True:
     event, values = window.read()
     print(event)
     print(values)
+    # print(list_box.GetIndexes()[0])
     match event:
         case "Add":
             todos = functions.get_todos()
             todos.append(values["todo"] + "\n")
             functions.write_todos(todos)
+            window['todo'].update(value="")
+            window['todos'].update(values=todos)
+        case "Edit":
+            todo_to_edit = values['todos'][0]
+            new_todo = values['todo']
+            todos = functions.get_todos()
+            index = todos.index(todo_to_edit)
+            todos[index] = new_todo + "\n"
+            functions.write_todos(todos)
+
+        case 'todos':
+            window['todo'].update(value=values['todos'][0])
         case sg.WIN_CLOSED:
             break
 
