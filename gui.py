7f25a458c2884e9228f69b0f51556df6df8fa5e9
SuperGluedGIT <sparkn@mac.com>
2024-07-31 21:20:05 -0400

    CAdded 'Add' and win.closed

--- gui.py
+++ gui.py
@@ -2,12 +2,25 @@ import functions
 import FreeSimpleGUI as sg
 
 label = sg.Text("Type in a to-do")
-input_box = sg.InputText(tooltip="enter todo")
+input_box = sg.InputText(tooltip="enter todo", key="todo")
 add_button = sg.Button("Add")
 
-window = sg.Window('My To-Do App', layout=[[label], [input_box, add_button]]
-                   )
-window.read()
-print("Hello")
+window = sg.Window('My To-Do App',
+                   layout=[[label], [input_box, add_button]],
+                   font=('Helvetica', 20))
+
+while True:
+
+    event, values = window.read()
+    print(event)
+    print(values)
+    match event:
+        case "Add":
+            todos = functions.get_todos()
+            todos.append(values["todo"] + "\n")
+            functions.write_todos(todos)
+        case sg.WIN_CLOSED:
+            break
+
 window.close()
 
