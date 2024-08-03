6da84765513a893338f7e6477ad8809a2a8d3d10
SuperGluedGIT <sparkn@mac.com>
2024-08-03 14:09:12 -0400

    error handling

--- gui.py
+++ gui.py
@@ -1,13 +1,14 @@
 import functions
 import FreeSimpleGUI as sg
 
+sg.theme('DarkBlue')
 
 label = sg.Text("Type in a to-do")
 input_box = sg.InputText(tooltip="enter todo", key="todo")
-add_button = sg.Button("Add")
+add_button = sg.Button("Add", font=('Helvetica', 15))
 list_box = sg.Listbox(values=functions.get_todos(), key="todos", enable_events=True, size=(44, 10))
-edit_button = sg.Button("Edit")
-complete_button = sg.Button("Complete")
+edit_button = sg.Button("Edit", font=('Helvetica', 15))
+complete_button = sg.Button("Complete", font=('Helvetica', 15))
 exit_button = sg.Button("Exit")
 
 window = sg.Window('My To-Do App',
