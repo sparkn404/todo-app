cb0c62d93f50cebd19f30f3632315f498b4a2454
SuperGluedGIT <sparkn@mac.com>
2024-08-06 21:09:46 -0400

    Added Add new item to web.py

--- web.py
+++ web.py
@@ -1,8 +1,16 @@
+# streamlit run web.py from Terminal
+
 import streamlit as st
 import functions
 
-todos = functions.get_todos()
 
+def add_todo():
+    todo_to_add = st.session_state["new_todo"] + "\n"
+    todos.append(todo_to_add)
+    functions.write_todos(todos)
+
+
+todos = functions.get_todos()
 
 st.title("My Todo App")
 st.subheader("This is my subheader")
@@ -11,4 +19,7 @@ st.write("This is a write function")
 for todo in todos:
     st.checkbox(todo)
 
-st.text_input(label='Add a todo:', placeholder='Add a todo:', label_visibility='hidden')
+st.text_input(label='Add a todo:', placeholder='Add a todo:',
+              on_change=add_todo, key='new_todo')
+
+st.session_state
